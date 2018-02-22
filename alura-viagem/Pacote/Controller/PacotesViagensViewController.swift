636a885f219cf6615526990293bb601ccc5e18d4
rodrigo <rodrigo.luizduarte@gmail.com>
2018-02-22 01:05:10 -0300

    Término da aula 2 do curso de  IOS layout parte 2.

---
+++ alura-viagem/Pacote/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,80 @@
+//
+//  PacotesViagensViewController.swift
+//  alura-viagem
+//
+//  Created by Rodrigo Luiz Duarte on 21/02/2018.
+//  Copyright © 2018 Rodrigo Luiz Duarte. All rights reserved.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource,
+                                    UICollectionViewDelegateFlowLayout,
+                                    UISearchBarDelegate {
+
+    @IBOutlet weak var pacotesCollectionView: UICollectionView!
+    @IBOutlet weak var searchBarPacotes: UISearchBar!
+    @IBOutlet weak var labelPacotesEncontrados: UILabel!
+    
+    let listaComTodasViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    var listaViagens:Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.labelPacotesEncontrados.text = self.atuslizaLabelPacotesEncontrados()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listaViagens.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        
+        let viagem = listaViagens[indexPath.row]
+        
+        celulaPacote.labelTitulo.text = viagem.titulo
+        celulaPacote.labelQuantidadeDeDias.text = "\(viagem.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(viagem.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagem.caminhoDaImagem)
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85/255, green: 85/255, blue: 85/255, alpha: 1).cgColor
+        celulaPacote.layer.cornerRadius = 10
+        
+        return celulaPacote
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: pacotesCollectionView.bounds.width / 2 - 15, height: 160)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        listaViagens = listaComTodasViagens
+        
+        if (searchText != "") {
+            let filtroListaViagem = NSPredicate(format: "titulo contains %@", searchText)
+            let listaFiltrada:Array<Viagem> = (listaViagens as NSArray).filtered(using: filtroListaViagem) as! Array
+            listaViagens = listaFiltrada
+        }
+        
+        self.labelPacotesEncontrados.text = self.atuslizaLabelPacotesEncontrados()
+        pacotesCollectionView.reloadData()
+    }
+    
+    func atuslizaLabelPacotesEncontrados() -> String {
+        
+        if (listaViagens.count == 0) {
+            return "Nenhum pacote encontrado"
+        } else {
+            return listaViagens.count == 1 ? "1 pacote encontrado" : "\(listaViagens.count) pacotes encontrados"
+        }
+    }
+}
---
+++ alura-viagem/Pacote/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  alura-viagem
+//
+//  Created by Rodrigo Luiz Duarte on 21/02/2018.
+//  Copyright © 2018 Rodrigo Luiz Duarte. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+}
